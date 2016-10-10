29d4dbef6f2a9d60f8a37a5f90f1dbb863949b43
Hamza Sheikh <code@codeghar.com>
2016-10-10 15:37:08 -0700

    Remove unused var

--- brokerlso/__init__.py
+++ brokerlso/__init__.py
@@ -2,6 +2,5 @@ import logging
 import sys
 
 FORMAT = "%(asctime)s : %(pathname)s : %(name)s : %(module)s : %(funcName)s : %(lineno)d : %(levelname)s : %(message)s"
-date_format = "%Y%m%d-%H%M%S-%f"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG, stream=sys.stdout)
 logger = logging.getLogger("brokerlso")
